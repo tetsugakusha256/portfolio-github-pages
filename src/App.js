adcdc286d313b1cc26bb5caf3fb1c6bceb17c625
Noobvelopper <fournierjulien01@gmail.com>
2022-10-16 00:20:00 +0900

    NavBar start

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Portfolio</h2>
+        <NavBar />
       </div>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NavBar() {
+  return <div>Futur NavBar V2</div>;
+}
